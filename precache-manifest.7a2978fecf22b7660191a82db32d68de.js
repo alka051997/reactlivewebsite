fa97a4eb132cceb222cff0ed57c84afa1cdf8982
alka051997 <alkakurrey5@gmail.com>
2020-12-16 22:46:58 +0530

    Updates

---
+++ precache-manifest.7a2978fecf22b7660191a82db32d68de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4abadf25ea84b47ec1",
+    "url": "/reactlivewebsite/static/css/main.89ed8729.chunk.css"
+  },
+  {
+    "revision": "1d4abadf25ea84b47ec1",
+    "url": "/reactlivewebsite/static/js/main.cfe22057.chunk.js"
+  },
+  {
+    "revision": "eab5b4adfd8e85082f5d",
+    "url": "/reactlivewebsite/static/js/runtime~main.5ade5b55.js"
+  },
+  {
+    "revision": "0c73c262770dc0e521d8",
+    "url": "/reactlivewebsite/static/css/2.bfdf8e10.chunk.css"
+  },
+  {
+    "revision": "0c73c262770dc0e521d8",
+    "url": "/reactlivewebsite/static/js/2.4659637e.chunk.js"
+  },
+  {
+    "revision": "5ae570f06f26208e9a81",
+    "url": "/reactlivewebsite/static/js/3.730c9cd1.chunk.js"
+  },
+  {
+    "revision": "8bedec6cd3cec1dbd499f1d1089f63a5",
+    "url": "/reactlivewebsite/static/media/banner.8bedec6c.png"
+  },
+  {
+    "revision": "d3c5df2dc83a346224ee0be18f111ebf",
+    "url": "/reactlivewebsite/static/media/star.d3c5df2d.svg"
+  },
+  {
+    "revision": "44d28d01f4c0b2fd8bbc304078a6a4eb",
+    "url": "/reactlivewebsite/static/media/place.44d28d01.svg"
+  },
+  {
+    "revision": "ea981b6c307213e0264e92bd41a4e64f",
+    "url": "/reactlivewebsite/static/media/week.ea981b6c.svg"
+  },
+  {
+    "revision": "0c4b283fefd538c58818c5a640e17222",
+    "url": "/reactlivewebsite/static/media/enroll.0c4b283f.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reactlivewebsite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "da6d496bd724bf8cd90fd9356ca86629",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
